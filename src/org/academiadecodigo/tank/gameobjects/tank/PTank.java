7b9339e06b99c69fc73f9359db7f724bc01d88dc
Tekman89 <tekman89@gmail.com>
2016-05-24 18:43:03 +0100

    Input

--- src/org/academiadecodigo/tank/gameobjects/tank/PTank.java
+++ src/org/academiadecodigo/tank/gameobjects/tank/PTank.java
@@ -1,10 +1,20 @@
 package org.academiadecodigo.tank.gameobjects.tank;
 
+import org.academiadecodigo.tank.grid.GridDirection;
+
 /**
  * Created by codecadet on 23/05/16.
  */
-public class PTank extends Tank {
+public class PTank extends Tank{
 
+    
+
+
+
+    @Override
+    public void move(GridDirection direction) {
+
+    }
 
 
 
