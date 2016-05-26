dc4a9a21daedf7d62f7e238b480cc4dadec1d72d
Sofia <ana.smirv@gmail.com>
2016-05-26 16:26:33 +0100

    Some more grid work

--- src/org/academiadecodigo/tank/gfx/simplegfx/SimpleGfxGrid.java
+++ src/org/academiadecodigo/tank/gfx/simplegfx/SimpleGfxGrid.java
@@ -1,5 +1,7 @@
 package org.academiadecodigo.tank.gfx.simplegfx;
 
+import org.academiadecodigo.simplegraphics.graphics.Color;
+import org.academiadecodigo.simplegraphics.graphics.Rectangle;
 import org.academiadecodigo.tank.grid.Grid;
 import org.academiadecodigo.tank.grid.position.GridPosition;
 
@@ -7,24 +9,45 @@ import org.academiadecodigo.tank.grid.position.GridPosition;
  * Created by codecadet on 24/05/16.
  */
 public class SimpleGfxGrid  implements Grid{
+
+
+    private Rectangle grid;
+    private int cols;
+    private int rows;
+
+    private int CELL_SIZE = 1;
+
+
+    public SimpleGfxGrid(int cols, int rows) {
+        this.cols = cols;
+        this.rows = rows;
+        grid = new Rectangle(0, 0, cols * CELL_SIZE, rows * CELL_SIZE);
+    }
+
+
+
     @Override
     public void init() {
-
+        grid.setColor(Color.GRAY);
+        grid.fill();
     }
 
     @Override
     public int getCols() {
-        return 0;
+        return cols;
     }
 
     @Override
     public int getRows() {
-        return 0;
+        return rows;
     }
+
+    public int getCellSize() { return CELL_SIZE; }
 
     @Override
     public GridPosition makeGridPosition() {
-        return null;
+        int
+        return new SimpleGfxGridPosition();
     }
 
     @Override
--- src/org/academiadecodigo/tank/gfx/simplegfx/SimpleGfxGridPosition.java
+++ src/org/academiadecodigo/tank/gfx/simplegfx/SimpleGfxGridPosition.java
@@ -1,6 +1,7 @@
 package org.academiadecodigo.tank.gfx.simplegfx;
 
 import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+import org.academiadecodigo.simplegraphics.graphics.Shape;
 import org.academiadecodigo.tank.grid.position.AbstractGridPosition;
 import org.academiadecodigo.tank.grid.GridColor;
 import org.academiadecodigo.tank.grid.position.GridPosition;
@@ -12,29 +13,33 @@ public class SimpleGfxGridPosition extends AbstractGridPosition {
 
     private int col;
     private int row;
-    private Rectangle rectangle;
+    private Shape shape;
 
     public SimpleGfxGridPosition (int col, int row, SimpleGfxGrid grid){
 
         super(col, row, grid);
 
-        rectangle = new Rectangle(toPixel(col),toPixel(row),100,100);
+        //shape = some shape
 
     }
 
     private int toPixel(int n){
-        return n * getGrid().getCellSize();
+        return n * ((SimpleGfxGrid)getGrid()).getCellSize();
     }
 
     @Override
     public int getCol() {
-        return 0;
+        return col;
     }
 
     @Override
     public int getRow() {
-        return 0;
+        return row;
     }
+
+
+    public void 
+
 
     @Override
     public void setPos(int col, int row) {
@@ -42,23 +47,12 @@ public class SimpleGfxGridPosition extends AbstractGridPosition {
     }
 
     @Override
-    public GridColor getColor() {
-        return null;
-    }
-
-    @Override
-    public void setColor(GridColor color) {
-
-    }
-
-    @Override
     public void show() {
-
     }
 
     @Override
     public void hide() {
-
+        shape.delete();
     }
 
     @Override
